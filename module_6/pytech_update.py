4d7f2826676b8f347904ec217b0b0e5409a4aad3
Nicolas Diamond <nicodiamond1c8@gmail.com>
2023-02-04 14:17:33 -0700

    Assignment 6.2
    Deleted test files and uploaded pytech_update.py

---
+++ module_6/pytech_update.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.nx2rkjl.mongodb.net/pytech"
+client = MongoClient(url)
+db = client.pytech
+
+students = db.students
+docs = students.find({})
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for doc in docs:
+    print('Student ID: %s' % doc["student_id"])
+    print('First Name: %s' % doc["first_name"])
+    print('Last Name: %s' % doc["last_name"])
+    print()
+
+result = students.update_one ({'student_id': 1007}, {'$set': {'last_name': "Smith"}})
+doc = students.find_one({"student_id": 1007})
+print("-- DISPLAYING STUDENT DOCUMENT 1007 --")
+
+print('Student ID: %s' % doc["student_id"])
+print('First Name: %s' % doc["first_name"])
+print('Last Name: %s' % doc["last_name"])
+print()
